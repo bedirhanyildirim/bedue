2f4eb2b79363463026492756c42810f8682d0c24
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2022-05-11 12:43:14 +0300

    fix: companies page css

--- src/pages/DashboardCompanies.vue
+++ src/pages/DashboardCompanies.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="mx-auto w-full max-w-screen-xl overflow-auto px-6">
-    <div class="flex items-center mb-6 justify-between">
-      <h1 class="text-2xl font-bold mt-6">Companies</h1>
+    <div class="flex items-center my-6 justify-between">
+      <h1 class="text-2xl font-bold">Companies</h1>
       <a href="#" class="text-xs font-medium text-white bg-blue-500 px-3 py-2 rounded hover:bg-blue-600 transition-colors">Add New</a>
     </div>
     <div class="alert primary mb-6">
