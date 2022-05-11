1f8e9a9a67e7e1442a04cd0c7287d338b374e2b8
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2022-05-11 08:43:31 +0300

    feat: navbar updated for auth

--- src/components/dashboard/layout/Navbar.vue
+++ src/components/dashboard/layout/Navbar.vue
@@ -3,13 +3,38 @@
       <router-link to="/" class="text-xl font-medium">
         NutritionSearch<span class="text-sm font-normal text-gray-500">.org</span>
       </router-link>
-      <div>Login / Logout</div>
+      <div v-if="!isLogedIn">
+        <router-link to="/login">Login</router-link> | 
+        <router-link to="/signup">Signup</router-link>
+      </div>
+      <div v-else>
+        <a href="#" @click="logout()">Logout</a>
+      </div>
     </header>
 </template>
 
 <script>
-export default {
+import { useStore } from "vuex"
+import { useRouter } from "vue-router"
+import { computed } from "@vue/runtime-core"
 
+export default {
+  setup() {
+    const store = useStore()
+    const router = useRouter()
+
+    const logout = async() => {
+      await store.dispatch('logout')
+      router.push('/login')
+    }
+
+    const isLogedIn = computed(() => store.getters.isLogedIn)
+
+    return {
+      logout,
+      isLogedIn
+    }
+  }
 }
 </script>
 
