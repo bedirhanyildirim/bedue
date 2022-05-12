064c95871f6485c7a5b9611943d5fe9c099986e6
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2022-05-12 09:29:02 +0300

    fix: new folder created for companies

--- src/pages/Dashboard/Companies/Companies.vue
+++ src/pages/Dashboard/Companies/Companies.vue
@@ -2,7 +2,10 @@
   <section class="mx-auto w-full max-w-screen-xl overflow-auto px-6">
     <div class="flex items-center my-6 justify-between">
       <h1 class="text-2xl font-bold">Companies</h1>
-      <a href="#" class="text-xs font-medium text-white bg-blue-500 px-3 py-2 rounded hover:bg-blue-600 transition-colors">Add New</a>
+      <router-link 
+        to="/dashboard/companies/new" 
+        class="text-xs font-medium text-white bg-blue-500 px-3 py-2 rounded hover:bg-blue-600 transition-colors"
+        >Add New</router-link>
     </div>
     <div class="alert primary mb-6">
       <span>This page helps you to find the list of the registered companies.</span>
@@ -12,7 +15,7 @@
 </template>
 
 <script>
-import companiesTableVue from "../components/dashboard/companies/companiesTable.vue"
+import companiesTableVue from "../../../components/dashboard/companies/companiesTable.vue"
 export default {
   components: {
     companiesTableVue
