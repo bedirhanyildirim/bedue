ff9aaf492002ac72306dd6bf6625737b840bd1cd
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2022-05-12 16:02:37 +0300

    feat: certificates page created

--- src/pages/Dashboard/Certificates/Certificates.vue
+++ src/pages/Dashboard/Certificates/Certificates.vue
@@ -3,18 +3,23 @@
     <div class="flex items-center my-6 justify-between">
       <h1 class="text-2xl font-bold">Certificates</h1>
       <router-link 
-        to="/dashboard/certificates/new" 
-        class="text-xs font-medium text-white bg-blue-500 px-3 py-2 rounded hover:bg-blue-600 transition-colors"
+        to="/dashboard/certificates" 
+        class="text-xs btn btn-primary"
         >Add New</router-link>
     </div>
     <div class="alert primary mb-6">
       <span>This page helps you to find the list of the valid certificates.</span>
     </div>
+    <certificatesTable></certificatesTable>
   </section>
 </template>
 
 <script>
+import certificatesTable from "../../../components/dashboard/certificates/certificatesTable.vue"
 export default {
+  components: {
+    certificatesTable
+  }
 }
 </script>
 
