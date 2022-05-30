1e5d2c0b32ceb60c31d7280972a35ad2bed13f4c
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2022-05-30 09:38:57 +0300

    feat: vue click away added

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import router from './router'
 import './index.css'
 import './firebase/index.js'
 import store from './store'
+import VueClickAway from 'vue3-click-away'
 
 const app = createApp(App)
 app.use(router)
 app.use(store)
+app.use(VueClickAway)
 app.mount('#app')
