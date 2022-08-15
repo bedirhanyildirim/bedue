8d5e438ac940f057b249e2fe36393929bb7f4619
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2022-08-15 06:09:16 +0200

    feat: navbar tr content added

--- src/components/frontend/layout/navbar.vue
+++ src/components/frontend/layout/navbar.vue
@@ -5,11 +5,13 @@
           <span class="text-purple-700 font-['Open_Sans']">Nutrition Search</span>
         </router-link>
         <div class="bg-white hidden lg:flex flex-row h-full items-center">
-          <router-link to="/" class="fe-nav-link">Home</router-link>
+          <!-- <router-link to="/" class="fe-nav-link">Home</router-link> -->
+          <router-link to="/" class="fe-nav-link">Anasayfa</router-link>
           <div class="relative dropdown flex h-full">
             <router-link to="/about" class="fe-nav-link">
               <div class="flex flex-row items-center justify-between">
-                <span>About</span>
+                <!-- <span>About</span> -->
+                <span>Hakkımızda</span>
                 <span class="ml-1">
                   <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
@@ -19,20 +21,24 @@
             </router-link>
             <ul id="fe-navbar-dropdown-1" class="absolute left-0 top-[96px] border w-auto bg-white hidden flex-col overflow-hidden transition-all">
               <li>
-                <router-link to="/about/FORCCW" class="fe-nav-link-dropdown">What is FORCCW</router-link>
+                <!-- <router-link to="/about/strategy" class="fe-nav-link-dropdown">STRATEGY</router-link> -->
+                <router-link to="/about/strategy" class="fe-nav-link-dropdown">STRATEJİ</router-link>
               </li>
               <li>
-                <router-link to="/about/quality-concerns" class="fe-nav-link-dropdown">Quality Concerns</router-link>
+                <!-- <router-link to="/about/mission" class="fe-nav-link-dropdown">Mission</router-link> -->
+                <router-link to="/about/mission" class="fe-nav-link-dropdown">Misyon</router-link>
               </li>
               <li>
-                <router-link to="/about/vision" class="fe-nav-link-dropdown">Vision & Mission</router-link>
+                <!-- <router-link to="/about/values" class="fe-nav-link-dropdown">VALUES</router-link> -->
+                <router-link to="/about/values" class="fe-nav-link-dropdown">DEĞERLERİMİZ</router-link>
               </li>
             </ul>
           </div>
           <div class="relative dropdown flex h-full">
             <router-link to="/quality" class="fe-nav-link">
               <div class="flex flex-row items-center justify-between">
-                <span>Quality</span>
+                <!-- <span>Quality</span> -->
+                <span>KALİTE</span>
                 <span class="ml-1">
                   <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
@@ -44,7 +50,8 @@
               <li class="border-b">
                 <router-link to="/quality/certificates" class="fe-nav-link-dropdown pb-2">
                   <div class="flex flex-row items-center justify-between">
-                    <span>Certificates</span>
+                    <!-- <span>Certificates</span> -->
+                    <span>SERTİFİKALAR</span>
                     <span class="ml-1">
                       <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                         <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
@@ -60,22 +67,29 @@
                     <router-link to="/quality/certificates/GKOC" class="fe-nav-link-dropdown-second">GKOC</router-link>
                   </li>
                   <li>
-                    <router-link to="/quality/certificates/GPRO" class="fe-nav-link-dropdown-second pb-3">GPRO</router-link>
+                    <router-link to="/quality/certificates/GPC" class="fe-nav-link-dropdown-second pb-3">GPC</router-link>
                   </li>
                 </ul>
               </li>
               <li class="border-b">
-                <router-link to="/quality/how-do-we-test" class="fe-nav-link-dropdown">How Do We Test</router-link>
+                <!-- <router-link to="/quality/how-do-we-test" class="fe-nav-link-dropdown">How Do We Test</router-link> -->
+                <router-link to="/quality/how-do-we-test" class="fe-nav-link-dropdown">TESTLER NASIL YAPILIR</router-link>
               </li>
               <li>
-                <router-link to="/quality/types" class="fe-nav-link-dropdown">Product Types</router-link>
+                <!-- <router-link to="/quality/types" class="fe-nav-link-dropdown">Product Types</router-link> -->
+                <router-link to="/quality/types" class="fe-nav-link-dropdown">ÜRÜN TÜRLERİ VE FORMATLARI</router-link>
               </li>
             </ul>
           </div>
-          <router-link to="/reports" class="fe-nav-link">Reports</router-link>
-          <router-link to="/faq" class="fe-nav-link">F.A.Q.</router-link>
-          <router-link to="/contact" class="fe-nav-link">Contact</router-link>
+          <!-- <router-link to="/reports" class="fe-nav-link">Reports</router-link> -->
+          <router-link to="/reports" class="fe-nav-link">RAPORLAR</router-link>
+          <!-- <router-link to="/faq" class="fe-nav-link">F.A.Q.</router-link> -->
+          <router-link to="/faq" class="fe-nav-link">S.S.S.</router-link>
+          <!-- <router-link to="/contact" class="fe-nav-link">Contact</router-link> -->
+          <router-link to="/contact" class="fe-nav-link">İLETİŞİM</router-link>
         </div>
+
+        <!-- Mobile Menu -->
         <div class="flex lg:hidden h-full items-center">
           <div @click="toggleMobileMenu">
             <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M3 18V16H21V18ZM3 13V11H21V13ZM3 8V6H21V8Z"/></svg>
@@ -90,10 +104,12 @@
                 </div>
               </div>
               <div class="flex flex-col w-2/3 md:w-1/2" @click="toggleMobileMenu">
-                <router-link to="/" class="fe-nav-link-m">Home</router-link>
+                <!-- <router-link to="/" class="fe-nav-link-m">Home</router-link> -->
+                <router-link to="/" class="fe-nav-link-m">ANASAYFA</router-link>
                 <router-link to="/about" class="fe-nav-link-m">
                   <div class="flex flex-row items-center justify-between">
-                    <span>About</span>
+                    <!-- <span>About</span> -->
+                    <span>HAKKIMIZDA</span>
                     <span class="ml-1">
                       <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                         <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
@@ -102,13 +118,17 @@
                   </div>
                 </router-link>
                 <div class="w-full flex flex-col ml-2">
-                  <router-link to="/about/FORCCW" class="fe-nav-link-m text-lg">What is FORCCW</router-link>
-                  <router-link to="/about/quality-concerns" class="fe-nav-link-m text-lg">Quality Concerns</router-link>
-                  <router-link to="/about/vision" class="fe-nav-link-m text-lg">Vision & Mission</router-link>
+                  <!-- <router-link to="/about/strategy" class="fe-nav-link-m text-lg">Strategy</router-link> -->
+                  <router-link to="/about/strategy" class="fe-nav-link-m text-lg">STRATEJİ</router-link>
+                  <!-- <router-link to="/about/mission" class="fe-nav-link-m text-lg">Mission</router-link> -->
+                  <router-link to="/about/mission" class="fe-nav-link-m text-lg">MİSYON</router-link>
+                  <!-- <router-link to="/about/values" class="fe-nav-link-m text-lg">Values</router-link> -->
+                  <router-link to="/about/values" class="fe-nav-link-m text-lg">DEĞERLERİMİZ</router-link>
                 </div>
                 <router-link to="/quality" class="fe-nav-link-m">
                   <div class="flex flex-row items-center justify-between">
-                    <span>Quality</span>
+                    <!-- <span>Quality</span> -->
+                    <span>KALİTE</span>
                     <span class="ml-1">
                       <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                         <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
@@ -117,13 +137,19 @@
                   </div>
                 </router-link>
                 <div class="w-full flex flex-col ml-2">
-                  <router-link to="/quality/certificates" class="fe-nav-link-m text-lg">Certificates</router-link>
-                  <router-link to="/about/quality-concerns" class="fe-nav-link-m text-lg">Quality Concerns</router-link>
-                  <router-link to="/about/vision" class="fe-nav-link-m text-lg">Vision & Mission</router-link>
+                  <!-- <router-link to="/quality/certificates" class="fe-nav-link-m text-lg">Certificates</router-link> -->
+                  <router-link to="/quality/certificates" class="fe-nav-link-m text-lg">SERTİFİKALAR</router-link>
+                  <!-- <router-link to="/quality/how-do-we-test" class="fe-nav-link-m text-lg">HOW DO WE TEST</router-link> -->
+                  <router-link to="/quality/how-do-we-test" class="fe-nav-link-m text-lg">TESTLER NASIL YAPILIR</router-link>
+                  <!-- <router-link to="/quality/types" class="fe-nav-link-m text-lg">Product Types</router-link> -->
+                  <router-link to="/quality/types" class="fe-nav-link-m text-lg">ÜRÜN TÜRLERİ VE FORMATLARI</router-link>
                 </div>
-                <router-link to="/reports" class="fe-nav-link-m">Reports</router-link>
-                <router-link to="/faq" class="fe-nav-link-m">F.A.Q.</router-link>
-                <router-link to="/contact" class="fe-nav-link-m">Contact</router-link>
+                <!-- <router-link to="/reports" class="fe-nav-link-m">Reports</router-link> -->
+                <router-link to="/reports" class="fe-nav-link-m">RAPORLAR</router-link>
+                <!-- <router-link to="/faq" class="fe-nav-link-m">F.A.Q.</router-link> -->
+                <router-link to="/faq" class="fe-nav-link-m">S.S.S.</router-link>
+                <!-- <router-link to="/contact" class="fe-nav-link-m">Contact</router-link> -->
+                <router-link to="/contact" class="fe-nav-link-m">İLETİŞİM</router-link>
               </div>
             </div>
           </div>
