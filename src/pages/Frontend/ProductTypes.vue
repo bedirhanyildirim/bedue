8052edcd0b05216adf61abdc6d18253579f33cfb
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2022-08-15 05:59:05 +0200

    feat: ProductTypes page tr content added

--- src/pages/Frontend/ProductTypes.vue
+++ src/pages/Frontend/ProductTypes.vue
@@ -1,56 +1,66 @@
 <template>
-  <section class="max-w-screen-xl w-full flex flex-col justify-between items-center">
-    <div class="w-full py-20">
-      <h1 class="flex flex-col uppercase text-7xl mb-4 font-bold">
-        Product Types
-      </h1>
-      <span class="text-black w-1/2 flex">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium eos, eius ipsum ratione a aspernatur. Dolor dolore rem tenetur aut harum inventore dignissimos, hic nihil commodi illum accusamus eveniet. Velit.
-      </span>
-      <div class="flex">
-        <div class="w-1/2">
-            <h2 class="mt-8 text-lg font-bold">Which Products</h2>
-            <ul class="mb-4 list-disc">
-                <li>Fish oil (salmon oil, cod liver oil)</li>
-                <li>Krill oil</li>
-                <li>Algae oil</li>
-                <li>MCT (medium-chain triglyceride) oil</li>
-                <li>Red yeast rice</li>
-                <li>Foods (bone broth, flour, corn, sugar, tapioca, chocolate)</li>
-                <li>Energy drinks</li>
-                <li>Juice</li>
-                <li>Whey and protein powders</li>
-                <li>Advanced ingredients (binders, powders, allulose, collagen peptides)</li>
-                <li>Whole or cut & sifted plant or specific plant parts</li>
-                <li>Oils</li>
-                <li>Extracts</li>
-                <li>Isolates</li>
-                <li>Emulsions</li>
-                <li>Topical cream, lotion, wax</li>
-                <li>Hard candies</li>
-                <li>Vaporizing liquid</li>
-            </ul>
-        </div>
-        <div class="w-1/2">
-            <h2 class="mt-8 text-lg font-bold">Which Types</h2>
-            <ul class="mb-4 list-disc">
-                <li>Capsules</li>
-                <li>Softgels</li>
-                <li>Tablets</li>
-                <li>Liquids</li>
-                <li>Gummies</li>
-                <li>Drops</li>
-                <li>Chewables</li>
-                <li>Mists</li>
-                <li>Powders</li>
-                <li>Syrups</li>
-                <li>Beverages</li>
-                <li>Food ingredients</li>
-            </ul>
+  <div class="w-full relative">
+    <section class="w-full flex justify-center relative">
+      <div class="absolute w-full top-0 bottom-0 bg-hero-image-01 bg-center bg-no-repeat bg-cover bg-origin-border saturate-50 object-cover"></div>
+      <div class="absolute w-full top-0 bottom-0 bg-white/50 lg:bg-white/30 saturate-100"></div>
+      <div class="z-[1] max-w-screen-md lg:max-w-screen-xl px-4 xl:px-0 w-full flex flex-col-reverse lg:flex-row justify-between lg:items-center py-20 saturate-100">
+        <div class="w-full lg:w-3/4">
+          <!-- <h1 class="uppercase text-center lg:text-left text-2xl md:text-5xl font-bold">Product Types</h1> -->
+          <h1 class="uppercase text-center lg:text-left text-2xl md:text-5xl font-bold">ÜRÜN TÜRLERİ VE FORMATLARI</h1>
         </div>
       </div>
-    </div>
-  </section>  
+    </section>
+    <section class="w-full flex flex-col justify-between items-center">
+      <div class="w-full max-w-screen-xl px-4 xl:px-0 py-10 md:py-20 flex flex-col-reverse md:flex-row justify-between">
+        <div class="w-full md:w-2/3 md:pr-4">
+          <h2 class="uppercase text-xl lg:text-2xl font-bold mt-10">
+            SERTİFİKA PROGRAMINA DAHİL EDİLEBİLECEK ÜRÜN TÜRLERİ
+          </h2>
+          <span class="text-black block my-4 text-lg text-justify">
+            <ul class="list-disc list-inside">
+              <li>Balık yağı (Somon yağı)</li>
+              <li>Krill yağı</li>
+              <li>Yosun yağı</li>
+              <li>MCT yağı</li>
+              <li>Kırmızı pirinç mayası</li>
+              <li>Gıdalar (kemik suyu, un, mısır, şeker, çikolata, tapiyoka)</li>
+              <li>Enerji içecekleri</li>
+              <li>Meyve suyu</li>
+              <li>Peynir altı suyu ve protein tozları</li>
+              <li>Gelişmiş bileşenler (kolajen peptitler, bağlayıcılar, tozlar, allüloz)</li>
+              <li>Bütün veya kesilmiş ve elenmiş bitki veya belirli bitki parçaları</li>
+              <li>Yağlar</li>
+              <li>Ekstraktlar</li>
+              <li>İzolatlar</li>
+              <li>Emülsiyonlar</li>
+              <li>Topikal krem, losyon, balmumu</li>
+              <li>Sert şekerler</li>
+              <li>Buharlaşan sıvı</li>
+            </ul>
+          </span>
+          <h2 class="uppercase text-xl lg:text-2xl font-bold mt-10">
+            SERTİFİKA PROGRAMINA DAHİL EDİLEBİLECEK ÜRÜNLER FORMATLARI
+          </h2>
+          <span class="text-black block my-4 text-lg text-justify">
+            <ul class="list-disc list-inside">
+              <li>Kapsüller</li>
+              <li>Softjeller</li>
+              <li>Tabletler</li>
+              <li>Likitler</li>
+              <li>Şuruplar</li>
+              <li>Sakızlar</li>
+              <li>Damlalar</li>
+              <li>Çiğnenebilir maddeler</li>
+              <li>Sisler</li>
+              <li>Tozlar</li>
+              <li>İçecekler</li>
+              <li>Gıda bileşenleri</li>
+            </ul>
+          </span>
+        </div>
+      </div>
+    </section>  
+  </div>
 </template>
 
 <script>
